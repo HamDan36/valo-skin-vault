7d1131c2bd4cb374548e4e3847cfbecde8379c09
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-04-22 20:49:45 -0600

    cleans up code from fixing searchBar

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,19 +1,3 @@
-// import React from "react";
-
-// function SearchBar() {
-//   return (
-//     <input
-//       type="text"
-//       placeholder="Search skins..."
-//       className="sticky top-0 px-4 py-2 rounded-lg border dark:border-gray-700 bg-white dark:bg-gray-700"
-//     />
-//   );
-// }
-
-// export default SearchBar;
-
-// searchbar.js
-
 // components/SearchBar.js
 import React from "react";
 
