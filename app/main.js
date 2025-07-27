4ead745d0715f1afa9c9be33228ce5b8237a7e74
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-07-27 17:50:00 -0600

    removes old unused code

--- app/main.js
+++ app/main.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './styles/global.css';
-import { CartProvider } from './context/CartContext';
-import { ThemeProvider } from './context/ThemeContext';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import './styles/global.css';
+// import { CartProvider } from './context/CartContext';
+// import { ThemeProvider } from './context/ThemeContext';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </ThemeProvider>
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ThemeProvider>
+//       <CartProvider>
+//         <App />
+//       </CartProvider>
+//     </ThemeProvider>
+//   </React.StrictMode>
+// );
