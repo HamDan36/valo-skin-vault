e9c4b43aca48795b7be0892430878d2596c9231f
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-07-27 17:10:40 -0600

    Fixes API issue
    places API issue and places filtering logic back into the page.js file

--- app/services/valorantAPI.js
+++ app/services/valorantAPI.js
@@ -1,21 +1,17 @@
 const API_BASE = "https://valorant-api.com/v1";
 
 export async function getAllSkins() {
-  try{
+  try {
     const response = await fetch(`${API_BASE}/weapons/skins`);
 
     if (!response.ok) {
-      throw new Error(`Error fetching skins from API: ${response.message}`);
+      throw new Error(`Error fetching skins from API: ${response.statusText}`);
     }
-  }
 
-  catch (error) {
-    console.error("Failed to fetch skins:", error);
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Failed to fetch skins from API service:", error);
     throw error;
-  } 
-
-  const data = await response.json();
-  const allSkins = data.data;
-
-  
+  }
 }
